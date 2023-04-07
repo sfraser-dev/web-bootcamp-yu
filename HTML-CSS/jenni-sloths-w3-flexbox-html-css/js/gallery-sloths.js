260afe3ceda1638c002db9dc60e454544374faf8
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-04-07 18:26:33 +0100

    adding more sloth images. adding new facebook image. fixing js code to use correct class

--- HTML-CSS/jenni-sloths-w3-flexbox-html-css/js/gallery-sloths.js
+++ HTML-CSS/jenni-sloths-w3-flexbox-html-css/js/gallery-sloths.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
-  for (let i of document.querySelectorAll(".gallery img")) {
+  for (let i of document.querySelectorAll(".gallery-sloths img")) {
     i.onclick = () => i.classList.toggle("full");
   }
 });
